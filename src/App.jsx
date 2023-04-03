48eb99142ad9949efcc3bfb91d543a7a8ffde8ae
xyugen <alex.arias.jorym@gmail.com>
2023-04-03 21:37:21 +0800

    feat: install and set up tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './tailwind.css'
+
+const App = () => {
+  return (
+    <div className='h-screen flex flex-col items-center font-pops bg-gray-900 text-gray-200'>
+      <h1 className=''>Test</h1>
+      App
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pops: "Poppins, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+}
+
