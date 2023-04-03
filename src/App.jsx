7cfcc04732e2d95351fb82eae787f2a73e4431a5
xyugen <alex.arias.jorym@gmail.com>
2023-04-04 01:50:28 +0800

    feat: basic functionalities and search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+
 import './tailwind.css'
+import SearchBar from './SearchBar';
 
 const App = () => {
+  const URL = "https://api.weatherapi.com/v1/";
+
+  const [weatherData, setWeatherData] = useState([]);
+
+  const [city, setCity] = useState('Manila');
+
+  const weatherStatus = async () => {
+    await axios.get(`${URL}current.json?key=${process.env.REACT_APP_API_KEY}&q=${encodeURIComponent(city)}&aqi=no`)
+      .then(response => {
+        setWeatherData(response.data);
+        console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  const handleSearchResults = (data) => {
+    setCity(`${data.name} ${data.country}`);
+    weatherStatus();
+    console.log(`CITY: `+city);
+  }
+
+  const formatTime = (datetime) => {
+    const date = new Date(datetime);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'pm' : 'am';
+    const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${formattedHours}:${formattedMinutes} ${ampm}`;
+  }  
+
   return (
     <div className='h-screen flex flex-col items-center bg-gray-50 font-pops p-10 text-gray-800'>
-      <div>
-        <input
-          type="text"
-          title='Search'
-          placeholder='Search...'
-          className='p-3 rounded-md bg-gray-800 text-white'
-        />
-        search icon
-      </div>
+      <SearchBar onSearchResults={(data) => handleSearchResults(data)} />
       
+      {weatherData['location'] &&
       <div className='mx-5 font-extralight flex justify-center flex-col h-full'>
         <div className='text-center mb-5'>
-          <h1 className='lg:text-5xl text-3xl'>Belgrade, Serbia</h1>
+          <h1 className='lg:text-5xl text-3xl'>{weatherData['location'].name}, {weatherData['location'].country}</h1>
           <p>Wed, 5 Sep 2020</p>
         </div>
 
         <div className='grid grid-cols-2 mb-2'>
           <div className='text-center'>
-            <p className='text-7xl font-bold'>23°C</p>
-            <p className='text-lg'>Partly Sunny</p>
-            <p>Update 1:48pm</p>
+            <p className='text-7xl font-bold'>{weatherData['current'].temp_c}°C</p>
+            <p className='text-lg'>{weatherData['current']['condition'].text}</p>
+            <p>Update {formatTime(weatherData['current'].last_updated)}</p>
           </div>
 
-          <div>
-            <img src="" alt="Weather" />
+          <div className='flex align-center justify-center'>
+            <img className='' src={weatherData['current']['condition'].icon} alt="Weather" />
           </div>
         </div>
         <div className='grid grid-cols-3 text-sm text-center'>
-          <p>Barometer 1009.0 mb</p>
-          <p>Feels like 25°C</p>
-          <p>Humidity 41%</p>
+          <p>Barometer {weatherData['current'].pressure_mb} mb</p>
+          <p>Feels like {weatherData['current'].feelslike_c}°C</p>
+          <p>Humidity {weatherData['current'].humidity}%</p>
         </div>
       </div>
+      }
     </div>
   )
 }
---
+++ src/SearchBar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+const SearchBar = ({ onSearchResults }) => {
+    const URL = "https://api.weatherapi.com/v1/";
+
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+
+    const searchQuery = async () => {
+        await axios.get(`${URL}search.json?key=${process.env.REACT_APP_API_KEY}&q=${encodeURIComponent(searchValue)}`)
+            .then(response => {
+                setSearchResult(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    const handleChange = (event) => {
+        const inputLength = event.target.value.length;
+        const results = document.querySelector('#results');
+
+        if (inputLength >= 3) {
+            setSearchValue(event.target.value);
+            searchQuery();
+            if (results)
+                results.style.opacity = 1;
+        } else if (inputLength < 3 && document.querySelector('#results')) {
+            setSearchResult([]);
+            if (results)
+                results.style.opacity = 0;
+        }
+    }
+
+    const handleSearchClick = (name, country) => {
+        const data = { name, country };
+        onSearchResults(data);
+        setSearchValue('');
+    }
+
+    return (
+        <div className='w-fit'>
+            <input
+                id='search'
+                type="text"
+                title='Search'
+                placeholder='Search...'
+                onChange={handleChange}
+                className='p-3 rounded-md bg-gray-800 text-white w-96'
+            />
+
+            {searchResult && searchValue.length > 2 &&
+                <div 
+                    id='results'
+                    className='absolute z-10 mt-2 w-96 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none fo'>
+                    {searchResult.map((item) => (
+                        <button
+                            className='p-2 w-full rounded-md hover:bg-gray-100'
+                            onClick={() => handleSearchClick(item.name, item.country)} >
+                            {item.name}, {item.country}
+                        </button>
+                    ))}
+                </div>
+            }
+        </div>
+    )
+}
+
+export default SearchBar
